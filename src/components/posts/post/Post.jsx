f421f012d01ebcfa7d1b53dde8bfa01072fe2a59
francisco casado romero <84054317+misamina30@users.noreply.github.com>
2023-01-03 13:09:56 -0300

    feat:se agrego el sidebar

--- src/components/posts/post/Post.jsx
+++ src/components/posts/post/Post.jsx
@@ -3,7 +3,7 @@ import styles from './Post.module.css';
 const Post = props => {
 	return (
 		<div className={styles.card}>
-			<img src={props.image} />
+			<img className={styles.images} src={props.image} />
 			<div className={styles.info}>
 				{props.category.map((cat, index) => (
 					<span key={index} className={styles.category}>
@@ -12,7 +12,7 @@ const Post = props => {
 				))}
 			</div>
 			<span className={styles.title}>{props.title}</span>
-			<hr />
+			<hr className={styles.hr} />
 			<span className={styles.time}>{props.time}</span>
 			<p className={styles.desciption}>{props.overview}</p>
 		</div>
